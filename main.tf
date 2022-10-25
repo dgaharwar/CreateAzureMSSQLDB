045252e84c79cb49ee06d8e658f335b4cd695d82
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-10-25 13:57:42 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,197 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.19.1"
+    }
+  }
+  required_version = ">= 0.14.1"
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+# provider "azurerm" {
+#   features {}
+
+#   subscription_id            = var.subscription_id_msp
+#   client_id                  = var.client_id
+#   client_secret              = var.client_secret
+#   tenant_id                  = var.tenant_id
+#   skip_provider_registration = true
+#   #auxiliary_tenant_ids = [ var.tenant_id ]
+
+#   alias = "mspsub"
+# }
+
+data "azurerm_virtual_network" "selected-vnet" {
+  name                = var.vNet
+  resource_group_name = var.resourceGroup
+}
+
+data "azurerm_subnet" "subnets" {
+  count                = length(data.azurerm_virtual_network.selected-vnet.subnets)
+  name                 = data.azurerm_virtual_network.selected-vnet.subnets[count.index]
+  virtual_network_name = var.vNet
+  resource_group_name  = var.resourceGroup
+}
+
+# data "azurerm_subnet" "admin-subnet" {
+#   provider             = azurerm.mspsub
+#   name                 = var.subnetOmhsDBA
+#   virtual_network_name = var.vNetOmhsDBA
+#   resource_group_name  = var.resourceGroupOmhsDBA
+# }
+
+data "azurerm_subnet" "selected-subnet" {
+  name                 = var.subnet
+  virtual_network_name = var.vNet
+  resource_group_name  = var.resourceGroup
+}
+
+data "azurerm_resource_group" "selected-rg" {
+  name = var.resourceGroup
+}
+
+resource "azurerm_mssql_server" "main-mssql-server" {
+  name                          = var.mssqlServerName
+  resource_group_name           = data.azurerm_resource_group.selected-rg.name
+  location                      = data.azurerm_resource_group.selected-rg.location
+  version                       = "12.0"
+  administrator_login           = var.adminUser
+  administrator_login_password  = var.adminPassword
+  minimum_tls_version           = "1.2"
+  public_network_access_enabled = false
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+  /*
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+  */
+  /*
+  tags = {
+    environment = "production"
+  }
+  */
+}
+
+# resource "azurerm_sql_virtual_network_rule" "sql-vnet-rule" {
+#   count               = length(data.azurerm_virtual_network.selected-vnet.subnets)
+#   name                = "${var.mssqlServerName}-vnet-rule-${count.index}"
+#   resource_group_name = data.azurerm_resource_group.selected-rg.name
+#   server_name         = azurerm_mssql_server.main-mssql-server.name
+#   subnet_id           = data.azurerm_subnet.subnets[count.index].id
+
+#   depends_on = [azurerm_mssql_server.main-mssql-server]
+# }
+
+# resource "azurerm_sql_virtual_network_rule" "sql-admin-vnet-rule" {
+#   name                = "omhs-admin-vnet-rule"
+#   resource_group_name = data.azurerm_resource_group.selected-rg.name
+#   server_name         = azurerm_mssql_server.main-mssql-server.name
+#   subnet_id           = data.azurerm_subnet.admin-subnet.id
+
+#   depends_on = [azurerm_mssql_server.main-mssql-server]
+# }
+
+resource "azurerm_mssql_database" "main-db" {
+  name         = var.mssqlDBName
+  server_id    = azurerm_mssql_server.main-mssql-server.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = var.maxSizeGB
+  //read_scale     = true
+  sku_name             = var.mssqlSKU
+  storage_account_type = "Geo"
+  zone_redundant       = true
+  // Below attributes need to be set for SKU General Purpose Serverless
+  min_capacity = substr(var.mssqlSKU, 0, 4) == "GP_S" ? var.minCapacity[var.mssqlSKU] : null
+
+  auto_pause_delay_in_minutes = substr(var.mssqlSKU, 0, 4) == "GP_S" ? 60 : null
+
+  short_term_retention_policy {
+    retention_days = var.backupRetentionDays
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+
+  /*
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+  */
+  /*
+  tags = {
+    environment = "production"
+  }
+  */
+}
+
+/*
+output "test" {
+  value = substr(var.mssqlSKU, 0, 4)
+}
+*/
+
+# resource "azurerm_sql_firewall_rule" "fw-rule" {
+#   name                = "${var.mssqlServerName}-fw-rule"
+#   resource_group_name = data.azurerm_resource_group.selected-rg.name
+#   server_name         = azurerm_mssql_server.main-mssql-server.name
+#   start_ip_address    = var.startIPAddress
+#   end_ip_address      = var.endIPAddress
+# }
+
+resource "azurerm_private_endpoint" "privateendpoint" {
+  name                = "${var.mssqlServerName}-ep"
+  location            = data.azurerm_resource_group.selected-rg.location
+  resource_group_name = data.azurerm_resource_group.selected-rg.name
+  subnet_id           = data.azurerm_subnet.selected-subnet.id
+
+  # private_dns_zone_group {
+  #   name = "privatednszonegroup"
+  #   private_dns_zone_ids = [azurerm_private_dns_zone.dnsprivatezone.id]
+  # }
+
+  private_service_connection {
+    name                           = "privateendpointconnection"
+    private_connection_resource_id = azurerm_mssql_server.main-mssql-server.id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
+
+output "privateIPaddress" {
+  value = azurerm_private_endpoint.privateendpoint.private_service_connection[0].private_ip_address
+}
---
+++ variables.tf
@@ -0,0 +1,204 @@
+# variable "subscription_id_msp" {
+#   type      = string
+#   sensitive = true
+#   #default = "<%= customOptions.azurergprep_subscriptionid %>"
+# }
+
+variable "subscription_id" {
+  type      = string
+  sensitive = true
+  #default = "<%= customOptions.azurergprep_subscriptionid %>"
+}
+
+variable "client_id" {
+  type      = string
+  sensitive = true
+  #default = "<%= customOptions.azurergprep_clientid %>"
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+  #default = "<%= customOptions.azurergprep_clientsecret %>"
+}
+
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+  #default = "<%= customOptions.azurergprep_tenantid %>"
+}
+
+variable "resourceGroup" {
+  description = "Resource Group"
+  type        = string
+  #default     = "terratest"
+}
+
+variable "mssqlServerName" {
+  description = "MSSQL Server Name"
+  type        = string
+  validation {
+    condition     = length(var.mssqlServerName) > 0 && length(var.mssqlServerName) <= 63
+    error_message = "Server name must be greater than 0 and less than or equal to 63 characters."
+  }
+
+  validation {
+    condition     = can(regex("^[a-z0-9]", var.mssqlServerName)) && can(regex("[a-z0-9]$", var.mssqlServerName)) && (!can(regex("[^a-z0-9-]", var.mssqlServerName)))
+    error_message = "Server name must contain only lower case letters, hyphens, and numbers. Server name must not start or end with a hypen."
+  }
+}
+
+/*
+variable "mssqlVersion" {
+  description = "MSSQL Version"
+  type        = string
+}
+*/
+variable "mssqlDBName" {
+  description = "MSSQL Database Name"
+  type        = string
+
+  validation {
+    condition     = length(var.mssqlDBName) > 0 && length(var.mssqlDBName) <= 128
+    error_message = "Database name must be less than or equal to 128 characters."
+  }
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9]", var.mssqlDBName)) && can(regex("[a-zA-Z0-9]$", var.mssqlDBName)) && (!can(regex("[^a-zA-Z0-9-]", var.mssqlDBName)))
+    error_message = "Database name must contain only letters, hyphens, and numbers. Database name must not start or end with a hypen."
+  }
+}
+
+variable "mssqlSKU" {
+  description = "MSSQL SKU"
+  type        = string
+}
+
+variable "minCapacity" {
+  type = map(string)
+  default = {
+    "GP_S_Gen5_1" = "0.5"
+    "GP_S_Gen5_2" = "0.5"
+    "GP_S_Gen5_4" = "0.5"
+    "GP_S_Gen5_6" = "0.75"
+    "GP_S_Gen5_8" = "1"
+    "GP_S_Gen5_10" = "1.25"
+    "GP_S_Gen5_12" = "1.5"
+    "GP_S_Gen5_14" = "1.75"
+    "GP_S_Gen5_16" = "2"
+  }
+}
+
+variable "adminUser" {
+  description = "Administrator User"
+  type        = string
+
+  validation {
+    condition     = (!contains(["admin", "administrator", "sa", "root", "dbmanager", "loginmanager", "dbo", "guest", "public"], var.adminUser))
+    error_message = "Administrator username must not contain a SQL Identifier or a typical system name."
+  }
+
+  validation {
+    condition     = can(regex("^[a-z0-9]", var.adminUser)) && can(regex("[a-z0-9]$", var.adminUser)) && (!can(regex("[^a-z0-9-]", var.adminUser)))
+    error_message = "Administrator username must contain only lower case letters, hyphens, and numbers. Username must not start or end with a hypen."
+  }
+
+}
+
+variable "adminPassword" {
+  description = "Administrator Password"
+  type        = string
+
+  validation {
+    condition     = length(var.adminPassword) >= 8 && length(var.adminPassword) <= 128
+    error_message = "Administrator password must be greater than or equal to 8 and less than or equal to 128 characters."
+  }
+
+  validation {
+    condition     = can(regex("[a-z]", var.adminPassword))
+    error_message = "Administrator password must contain at least one lower case letter."
+  }
+
+  validation {
+    condition     = can(regex("[A-Z]", var.adminPassword))
+    error_message = "Administrator password must contain at least one upper case letter."
+  }
+
+  validation {
+    condition     = can(regex("[0-9]", var.adminPassword))
+    error_message = "Administrator password must contain at least one number."
+  }
+
+  validation {
+    condition     = can(regex("[^a-zA-Z0-9_]", var.adminPassword))
+    error_message = "Administrator password must contain at least one special character."
+  }
+
+}
+
+# variable "startIPAddress" {
+#   description = "Start IP address to allow access to SQL Server."
+#   type        = string
+
+#   validation {
+#     condition     = can(regex("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.startIPAddress))
+#     error_message = "Invalid start IP address provided."
+#   }
+# }
+
+# variable "endIPAddress" {
+#   description = "End IP address to allow access to SQL Server."
+#   type        = string
+
+#   validation {
+#     condition     = can(regex("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.endIPAddress))
+#     error_message = "Invalid end IP address provided."
+#   }
+# }
+
+variable "maxSizeGB" {
+  description = "Maximum size of Database(GB)"
+  type        = number
+
+  validation {
+    condition     = var.maxSizeGB > 0 && var.maxSizeGB <= 1000
+    error_message = "The storage size must be greater than 0 and less than or equal to 1000 GB."
+  }
+}
+
+variable "backupRetentionDays" {
+  description = "Backup Retention Days between 7 and 35"
+  type        = number
+
+  validation {
+    condition     = var.backupRetentionDays >= 7 && var.backupRetentionDays <= 35
+    error_message = "Backup retention days must be greater than or equal to 7 and less than or equal to 35 days."
+  }
+}
+
+variable "vNet" {
+  description = "Virtual Network"
+  type        = string
+}
+
+variable "subnet" {
+  description = "Subnet"
+  type        = string
+}
+
+# variable "resourceGroupOmhsDBA" {
+#   description = "Resource Group"
+#   type        = string
+#   default     = "ocio-omhs-prd-moderate-rg"
+# }
+
+# variable "vNetOmhsDBA" {
+#   description = "Virtual Network"
+#   type        = string
+#   default     = "ocio-omhs-prd-moderate-vnet"
+# }
+# variable "subnetOmhsDBA" {
+#   description = "OMHS DBA Subnet"
+#   type        = string
+#   default     = "ocio-omhs-prd-moderate-app-subnet"
+# }
